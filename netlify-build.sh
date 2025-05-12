4e0883bdd891661abef8160992ec31ac721f8453
Arjun K <arjunk9135@gmail.com>
2025-05-12 21:36:36 +0530

    netlify build:3

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-npm install -g vite
-npm install
+set -e # Exit on error
+
+# Clean node_modules and reinstall
+rm -rf node_modules package-lock.json
+npm ci --no-audit
+
+# Build the project
 npm run build
